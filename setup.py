8d9d7c0c5f05727e6845adb0b349342077ee3814
Amartya Sanyal <asanyal@twitter.com>
2017-08-05 18:53:54 -0400

    setup added

---
+++ setup.py
@@ -0,0 +1,65 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == "":
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+ 
+package_dir = "particles"
+ 
+packages = []
+for dirpath, dirnames, filenames in os.walk(package_dir):
+    # ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    if "__init__.py" in filenames:
+        packages.append(".".join(fullsplit(dirpath)))
+
+package_dir = 'tests'
+for dirpath, dirnames, filenames in os.walk(package_dir):
+    # ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    if "__init__.py" in filenames:
+        packages.append(".".join(fullsplit(dirpath)))
+
+setup(
+    name = "easyGrad",
+    version = "0.0.1",
+    author = "Amartya Sanyal",
+    author_email = "amartya18x@gmail.com",
+    description = ("A python package to do auto differentiation."),
+    license = "BSD",
+    keywords = "autodiff machine-learning deep learning",
+    url = "http://packages.python.org/easyGrad",
+    packages = packages,
+    long_description=read('README.md'),
+    dependency_links = [],
+    classifiers=[
+        'Development Status :: 4 - beta'
+        'Intended Audience :: Researchers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=['numpy']
+)
